969ee4c294f144c56c1146e7612e3593988a139e
Daniel Chen <Maveri2k@yahoo.com>
2016-02-28 20:16:09 -0500

    replace "Sem registros" (No records) with np.nan

--- src/buzzfeed/clean_parsed_brazil.py
+++ src/buzzfeed/clean_parsed_brazil.py
@@ -9,6 +9,7 @@ import re
 
 import pandas as pd
 import unicodedata
+import numpy as np
 
 
 def strip_accents(s):
@@ -50,6 +51,7 @@ def get_location_type(df, cdc_location_df):
 
 def clean_data(df):
     df['state_no_accents'] = df['state'].apply(strip_accents)
+    df = df.replace(r'\s*[Ss]em registros\s*', np.nan, regex=True)
     return df
 
 
